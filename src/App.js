fa8183eaf7a500949e1a5b28de2b90362348e9d6
Abrar Aslam <66551058+Abrar-aslam@users.noreply.github.com>
2023-04-08 17:45:29 +0530

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import './App.css';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+// import Home from "./pages/Home";
+
+
+
 
 function App() {
   return (
-  //  <h1>Hello</h1>
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+    //  <h1>Hello</h1>
+    // <div className="App">
+    //   <h1>Hello World</h1>
+    // </div>
   );
 }
 
